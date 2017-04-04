7aed2c3b3176803e685b0b4f34b1ca0e51000026
Chris Bay <chris@chrisbay.net>
2017-04-04 15:24:44 -0500

    Initial commit

---
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        private CheeseDbContext context;
+
+        public CheeseController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Cheese> cheeses = context.Cheeses.ToList();
+
+            return View(cheeses);
+        }
+
+        public IActionResult Add()
+        {
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            return View(addCheeseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                Cheese newCheese = new Cheese
+                {
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description,
+                    Type = addCheeseViewModel.Type
+                };
+
+                context.Cheeses.Add(newCheese);
+                context.SaveChanges();
+
+                return Redirect("/Cheese");
+            }
+
+            return View(addCheeseViewModel);
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheeses = context.Cheeses.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] cheeseIds)
+        {
+            foreach (int cheeseId in cheeseIds)
+            {
+                Cheese theCheese = context.Cheeses.Single(c => c.ID == cheeseId);
+                context.Cheeses.Remove(theCheese);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ src/CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,10 @@
+﻿namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public CheeseType Type { get; set; }
+        public int ID { get; set; }
+    }
+}
